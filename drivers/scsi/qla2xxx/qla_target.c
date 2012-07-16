38b86951eb3f438c9d38532504088514a2bd9009
Roland Dreier <roland@purestorage.com>
2012-07-15 18:00:18 -0700

    qla2xxx: Get rid of redundant qla_tgt_sess.tearing_down
    The only place that sets qla_tgt_sess.tearing_down calls
    target_splice_sess_cmd_list() immediately afterwards, without dropping
    the lock it holds.  That function sets se_session.sess_tearing_down,
    so we can get rid of the qla_target-specific flag, and in the one
    place that looks at the qla_tgt_sess.tearing_down flag just test
    se_session.sess_tearing_down instead.
    
    Signed-off-by: Roland Dreier <roland@purestorage.com>

--- drivers/scsi/qla2xxx/qla_target.c
+++ drivers/scsi/qla2xxx/qla_target.c
@@ -2508,7 +2508,7 @@ static void qla_tgt_do_work(struct work_struct *work)
 	sess = ha->tgt_ops->find_sess_by_s_id(vha,
 				atio->u.isp24.fcp_hdr.s_id);
 	if (sess) {
-		if (unlikely(sess->tearing_down)) {
+		if (unlikely(sess->se_sess->sess_tearing_down)) {
 			sess = NULL;
 			spin_unlock_irqrestore(&ha->hardware_lock, flags);
 			goto out_term;
--- drivers/scsi/qla2xxx/tcm_qla2xxx.c
+++ drivers/scsi/qla2xxx/tcm_qla2xxx.c
@@ -463,7 +463,6 @@ static int tcm_qla2xxx_shutdown_session(struct se_session *se_sess)
 	vha = sess->vha;
 
 	spin_lock_irqsave(&vha->hw->hardware_lock, flags);
-	sess->tearing_down = 1;
 	target_splice_sess_cmd_list(se_sess);
 	spin_unlock_irqrestore(&vha->hw->hardware_lock, flags);
 
@@ -927,7 +926,6 @@ static void tcm_qla2xxx_put_sess(struct qla_tgt_sess *sess)
 
 static void tcm_qla2xxx_shutdown_sess(struct qla_tgt_sess *sess)
 {
-	sess->tearing_down = 1;
 	target_splice_sess_cmd_list(sess->se_sess);
 }
 
