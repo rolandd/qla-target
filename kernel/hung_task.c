72be7ff122ddfd05021ab8190c93719e8a45a93a
Joern Engel <joern@logfs.org>
2012-09-05 08:39:46 -0700

    hung_task: Dump every kernel stack
    For cases when the hung task is waiting for a mutex, it doesn't really
    help much to know its backtrace.  Hunting down the task that actually
    holds the blocking mutex makes much more sense.  So let's use a bit of
    brute force and just dump every stack in the system.
    
    Signed-off-by: Joern Engel <joern@logfs.org>

--- kernel/hung_task.c
+++ kernel/hung_task.c
@@ -128,8 +128,10 @@ static void check_hung_task(struct task_struct *t, unsigned long timeout)
 
 	touch_nmi_watchdog();
 
-	if (sysctl_hung_task_panic)
+	if (sysctl_hung_task_panic) {
+		show_state();
 		panic("hung_task: blocked tasks");
+	}
 }
 
 /*
