af141ada92a43ab6fc9c124a279ea27c5a545a07
Roland Dreier <roland@purestorage.com>
2013-01-21 09:35:35 -0800

    qla_target-dont-leak-terminated-commands
    patch status: sent upstream

--- drivers/scsi/qla2xxx/qla_target.c
+++ drivers/scsi/qla2xxx/qla_target.c
@@ -2588,10 +2588,12 @@ static void qla_tgt_do_work(struct work_struct *work)
 out_term:
 	ql_dbg(ql_dbg_tgt_mgt, vha, 0xe14d, "Terminating work cmd %p", cmd);
 	/*
-	 * cmd has not sent to target yet, so pass NULL as the second argument
+	 * cmd has not sent to target yet, so pass NULL as the second
+	 * argument to qla_tgt_send_term_exchange() and free it ourselves.
 	 */
 	spin_lock_irqsave(&ha->hardware_lock, flags);
 	qla_tgt_send_term_exchange(vha, NULL, &cmd->atio, 1);
+	kmem_cache_free(qla_tgt_cmd_cachep, cmd);
 	if (sess)
 		ha->tgt_ops->put_sess(sess);
 	spin_unlock_irqrestore(&ha->hardware_lock, flags);
